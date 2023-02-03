071a5e4a5e6fb78cb133c53309843eceb988334e
Teodor Heggelund <git@teod.eu>
2023-02-03 18:01:01 +0100

    Draft rest of the install script

--- install.sh
+++ install.sh
@@ -12,3 +12,14 @@ bash <(curl https://raw.githubusercontent.com/babashka/babashka/master/install)
 # TODO get version first, then download that version
 mkdir -p ~/.babashka/bbin/bin && curl -o- -L https://raw.githubusercontent.com/babashka/bbin/v0.1.2/bbin > ~/.babashka/bbin/bin/bbin && chmod +x ~/.babashka/bbin/bin/bbin
 # TODO sanely add to PATH -- check first if we need to.
+# if bbin path is not on PATH
+#   if shell is bash
+#       ...
+#   else if shell is fish
+#       ...
+#   end
+# end
+# From bbin install instructions:
+#   echo 'export PATH="$PATH:$HOME/.babashka/bbin/bin"' >> ~/.$(basename $SHELL)rc && exec $SHELL
+#
+# We need to add $HOME/.babashka/bbin/bin to PATH!
