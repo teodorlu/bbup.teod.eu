d56b260affa7c972be33296ca0fdb95a59da93b4
Teodor Heggelund <git@teod.eu>
2023-02-03 17:59:12 +0100

    Aaaand install babashka.

--- install.sh
+++ install.sh
@@ -8,3 +8,7 @@
 
 # Defer to default "babashka manual install"
 bash <(curl https://raw.githubusercontent.com/babashka/babashka/master/install)
+
+# TODO get version first, then download that version
+mkdir -p ~/.babashka/bbin/bin && curl -o- -L https://raw.githubusercontent.com/babashka/bbin/v0.1.2/bbin > ~/.babashka/bbin/bin/bbin && chmod +x ~/.babashka/bbin/bin/bbin
+# TODO sanely add to PATH -- check first if we need to.
