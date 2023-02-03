2d5805793aff07d0ae75977f3a4b7205199635fb
Teodor Heggelund <git@teod.eu>
2023-02-03 18:14:10 +0100

    Refactor: split into functions

--- install.sh
+++ install.sh
@@ -6,20 +6,29 @@
 # also have the Homebrew option. This installer does /not/ try to install with
 # homebrew.
 
-# Defer to default "babashka manual install"
-bash <(curl https://raw.githubusercontent.com/babashka/babashka/master/install)
+install_babashka() {
+    local dry_run="$1"
+    # Defer to default "babashka manual install"
+    bash <(curl https://raw.githubusercontent.com/babashka/babashka/master/install)
+}
 
-# TODO get version first, then download that version
-mkdir -p ~/.babashka/bbin/bin && curl -o- -L https://raw.githubusercontent.com/babashka/bbin/v0.1.2/bbin > ~/.babashka/bbin/bin/bbin && chmod +x ~/.babashka/bbin/bin/bbin
-# TODO sanely add to PATH -- check first if we need to.
-# if bbin path is not on PATH
-#   if shell is bash
-#       ...
-#   else if shell is fish
-#       ...
-#   end
-# end
-# From bbin install instructions:
-#   echo 'export PATH="$PATH:$HOME/.babashka/bbin/bin"' >> ~/.$(basename $SHELL)rc && exec $SHELL
-#
-# We need to add $HOME/.babashka/bbin/bin to PATH!
+install_bbin() {
+    local dry_run="$1"
+    # TODO get version first, then download that version
+    mkdir -p ~/.babashka/bbin/bin && curl -o- -L https://raw.githubusercontent.com/babashka/bbin/v0.1.2/bbin > ~/.babashka/bbin/bin/bbin && chmod +x ~/.babashka/bbin/bin/bbin
+    # TODO sanely add to PATH -- check first if we need to.
+    # if bbin path is not on PATH
+    #   if shell is bash
+    #       ...
+    #   else if shell is fish
+    #       ...
+    #   end
+    # end
+    # From bbin install instructions:
+    #   echo 'export PATH="$PATH:$HOME/.babashka/bbin/bin"' >> ~/.$(basename $SHELL)rc && exec $SHELL
+    #
+    # We need to add $HOME/.babashka/bbin/bin to PATH!
+}
+
+install_babashka
+install_bbin
